d71fd1fec04a88629e3ac1fe304fca59a04ba3ad
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-04-03 16:41:20 +0000

    prevented deletion of practice exam and debugged case where user submits with nil in form.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/practice/:id', to: 'exams#practice', as: 'practice'
   post '/exam_submission', to: 'practice_exams#submit_practice', as: 'exam_submission'
   resources :assembled_exam_questions
-  resources :practice_exams do
+  resources :practice_exams, except: [ :destroy] do
     member do
       post 'start_practice'
       post 'submit_practice'
