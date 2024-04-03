ae6c285696fb107dfbcde0bbea93b10012a1795a
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-04-03 16:02:42 +0000

    removed user access to question and question choices route and actions.

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,15 @@ Rails.application.routes.draw do
       post 'submit_practice'
     end
   end
-  resources :question_choices
-  resources :questions
+
+  # Restricting these routes by commenting.
+  #resources :question_choices 
+  #resources :questions
+  
   get 'landing/index'
   root "landing#index"
   resources :exams do
     post 'start_practice', on: :member
   end
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
