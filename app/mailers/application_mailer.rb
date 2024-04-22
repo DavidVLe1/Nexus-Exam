78019e5639cbd1809be9a5247334f1f9f3702363
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-04-22 20:08:14 +0000

    added production mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
+
+  after_action :log_email
+
+  private
+
+  def log_email
+    Rails.logger.info("Sent email to: #{mail.to}")
+  end
 end
